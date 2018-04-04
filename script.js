7099750e0c529d203459ce08acbd71c65735e959
more600D <s.s.sh@mail.ru>
2018-04-04 21:52:50 +0300

    решено!

--- script.js
+++ script.js
@@ -10,19 +10,37 @@ window.onload = function() {
             if(btn[i].getAttribute('name') == 'plus') {
                 var res = parseInt(num1.value) + parseInt(num2.value);
                 span.innerHTML = res;
+
+                $Enabled(btn[i]);
             }
             else if (btn[i].getAttribute('name') == 'minus'){
                 var res = parseInt(num1.value) - parseInt(num2.value);
                 span.innerHTML = res; 
+
+                $Enabled(btn[i]);
             } 
             else if (btn[i].getAttribute('name') == 'multy'){
                 var res = parseInt(num1.value) * parseInt(num2.value);
                 span.innerHTML = res; 
+
+                $Enabled(btn[i]);
             } 
             else if (btn[i].getAttribute('name') == 'del'){
                 var res = parseInt(num1.value) / parseInt(num2.value);
-                span.innerHTML = res; 
+                span.innerHTML = res;
+                
+                $Enabled(btn[i]);
             } 
         }
-    }     
+    }
+    
+    function $Enabled(a) {
+        a.setAttribute('disabled', 'disabled');
+        num1.oninput = function(){
+            a.removeAttribute('disabled');
+        }
+        num2.oninput = function(){
+            a.removeAttribute('disabled');
+        }
+    }
 }

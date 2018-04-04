7ffd2d0d4b22012d657ffd33f2da716b478f9a72
more600D <s.s.sh@mail.ru>
2018-04-04 15:05:22 +0300

    сложение

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 window.onload = function() {
 
-    
-    
+    var btn = document.querySelector('input[name=plus]');
+    var num1 = document.querySelector('input[name=num1]');
+    var num2 = document.querySelector('input[name=num2]');
+    var span = document.querySelector('.res');
+
+    btn.onclick = function(){
+
+        var res = parseInt(num1.value) + parseInt(num2.value);
+        if(res == NaN) {
+            span.innerHTML = 'Введите числа!';
+        }
+        else {
+            span.innerHTML = res;        
+        }
+  
+    }
 }
